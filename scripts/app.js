b0443846356b621e2abe5eab4712ebaba4b3314f
Cahit YIGIT <yigitc@netas.com.tr>
2019-09-19 13:59:00 +0300

    added device open and device close buttons to write data to the device

--- scripts/app.js
+++ scripts/app.js
@@ -1,66 +1,72 @@
 
 "use strict";
 
-var React = require('react');
-var ReactDOM = require('react-dom');
+const React         = require('react');
+const ReactDOM      = require('react-dom');
 
-var Row = require('react-bootstrap').Row;
-var Col = require('react-bootstrap').Col;
-var Grid = require('react-bootstrap').Grid;
-var Table = require('react-bootstrap').Table;
+const Row           = require('react-bootstrap').Row;
+const Col           = require('react-bootstrap').Col;
+const Grid          = require('react-bootstrap').Grid;
+const Table         = require('react-bootstrap').Table;
 
-var HID = require('node-hid');
+const HID           = require('node-hid');
 
-var devices = HID.devices();
+const devices = HID.devices();
 
-var App = React.createClass({
+const App = () => {
 
-    getInitialState: function() {
-        return {
-        };
-    },
-
-    render: function() {
-        var createDeviceRow = function(dev, index) {
-            return (
-                <tr key={index}>
-                    <td>{dev.vendorId}</td>
-                    <td>{dev.productId}</td>
-                    <td>{dev.product}</td>
-                    <td>{dev.manufacturer}</td>
-                    <td>{dev.serialNumber}</td>
-                    <td>{dev.path}</td>
-                </tr>
-            );
-        };
+    const createDeviceRow = (dev, index) => {
+        let deviceHandle = null;
 
         return (
-            <Grid>
-               <h1> Electron HID Toy! </h1>
-
-                <Row>
-                    <Col xs={12}>
-                        <h4> HID Devices connected </h4>
-                        <Table bordered condensed hover >
-    						<thead>
-    							<tr>
-                                    <th>VendorId</th>
-    								<th>ProductId</th>
-    								<th>Product</th>
-    								<th>Manufacturer</th>
-    								<th>SerialNumber</th>
-    								<th>Path</th>
-    							</tr>
-    						</thead>
-    						<tbody>
-    							{devices.map( createDeviceRow, this )}
-    						</tbody>
-    					</Table>
-                    </Col>
-                </Row>
-            </Grid>
+            <tr key={index}>
+                <td>{dev.vendorId}</td>
+                <td>{dev.productId}</td>
+                <td>{dev.product}</td>
+                <td>{dev.manufacturer}</td>
+                <td>{dev.serialNumber}</td>
+                <td>{dev.path}</td>
+                <td>
+                    <button onClick={ () => {
+                        deviceHandle = new HID.HID(dev.path);
+                        deviceHandle.write([0x02, 0x04]);
+                    } }>Open Device</button>
+                </td>
+                <td>
+                    <button onClick={ () => {
+                        deviceHandle.write([0x02, 0x00]);
+                    } }>Close Device</button>
+                </td>
+            </tr>
         );
-    }
-});
+    };
+
+    return (
+        <Grid>
+            <h1> Electron HID Toy! </h1>
+
+            <Row>
+                <Col xs={12}>
+                    <h4> HID Devices connected </h4>
+                    <Table bordered condensed hover >
+                        <thead>
+                            <tr>
+                                <th>VendorId</th>
+                                <th>ProductId</th>
+                                <th>Product</th>
+                                <th>Manufacturer</th>
+                                <th>SerialNumber</th>
+                                <th>Path</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {devices.map( createDeviceRow, this )}
+                        </tbody>
+                    </Table>
+                </Col>
+            </Row>
+        </Grid>
+    );
+}
 
 ReactDOM.render( <App />,  document.getElementById('example') );
