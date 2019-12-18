71db6fc76d2ffff97869fefa9f371dd1bfe1a021
Cahit YIGIT <yigitc@netas.com.tr>
2019-12-18 09:44:56 +0300

    added subscription to devices events

--- scripts/app.js
+++ scripts/app.js
@@ -13,6 +13,16 @@ const HID           = require('node-hid');
 
 const devices = HID.devices();
 
+const setupDeviceEventHandlers = (selectedDeviceHandle) => {
+    selectedDeviceHandle.on('data', deviceData => {
+        console.log(Date.now().toLocaleString(), JSON.stringify(deviceData, null, 4));
+    });
+
+    selectedDeviceHandle.on('error', deviceError => {
+        console.log(Date.now().toLocaleString(), JSON.stringify(deviceError, null, 4));
+    });
+}
+
 const App = () => {
 
     const createDeviceRow = (dev, index) => {
@@ -29,7 +39,7 @@ const App = () => {
                 <td>
                     <button onClick={ () => {
                         deviceHandle = new HID.HID(dev.path);
-                        deviceHandle.write([0x02, 0x04]);
+                        deviceHandle.write([0x02, 0x01]);
                     } }>Open Device</button>
                 </td>
                 <td>
@@ -37,7 +47,13 @@ const App = () => {
                         deviceHandle.write([0x02, 0x00]);
                     } }>Close Device</button>
                 </td>
-                <td>{JSON.stringify(dev, null, 4)}</td>
+                <td>
+                    <button onClick={ () => {
+                        deviceHandle = new HID.HID(dev.path);
+                        setupDeviceEventHandlers(deviceHandle);
+                    } }>Subscribe Device Events</button>
+                </td>
+                <td>{ JSON.stringify(dev, null, 4) }</td>
             </tr>
         );
     };
@@ -58,8 +74,9 @@ const App = () => {
                                 <th>Manufacturer</th>
                                 <th>SerialNumber</th>
                                 <th>Path</th>
-                                <th>Action</th>
-                                <th>Action</th>
+                                <th>Action 1</th>
+                                <th>Action 2</th>
+                                <th>Action 3</th>
                                 <th>Device Obj</th>
                             </tr>
                         </thead>
